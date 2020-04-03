2657531e0beefafbe79d462d3437599902faddac
Max Cohen <max.zagouri@pm.me>
2020-04-03 13:20:16 +0200

    Add csv to npz convert function

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import json
+
+import torch
+import pandas as pd
+import numpy as np
+
+def csv2npz(dataset_x_path, dataset_y_path, labels_path='labels.json'):
+    """Load input dataset from csv and create x_train tensor."""
+    # Load dataset as csv
+    x = pd.read_csv(dataset_x_path)
+    y = pd.read_csv(dataset_y_path)
+
+    # Load labels, file can be found in challenge description
+    with open(labels_path, "r") as stream_json:
+        labels = json.load(stream_json)
+
+    m = x.shape[0]
+    K = 672  # Can be found through csv
+
+    # Create R and Z
+    R = x[labels["R"]].values
+    R = R.astype(np.float32)
+
+    X = y[[f"{var_name}_{i}" for var_name in labels["X"]
+            for i in range(K)]]
+    X = X.values.reshape((m, -1, K))
+    X = X.astype(np.float32)
+
+    Z = x[[f"{var_name}_{i}" for var_name in labels["Z"]
+            for i in range(K)]]
+    Z = Z.values.reshape((m, -1, K))
+#     Z = Z.transpose((0, 2, 1))
+    Z = Z.astype(np.float32)
+    
+    np.savez('dataset.npz', R=R, X=X, Z=Z)
